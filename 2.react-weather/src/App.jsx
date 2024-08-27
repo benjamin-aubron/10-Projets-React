ac541cd4a739e47d53990462420213983bafc15f
Benjamin AUBRON <benjamin.aubron@outlook.com>
2024-08-27 11:46:16 +0200

    html & css - done

--- 2.react-weather/src/App.jsx
+++ 2.react-weather/src/App.jsx
@@ -1,9 +1,19 @@
+import loader from "./assets/loader.svg"
+import "./App.css"
 function App() {
  
   return (
 
       <main>
-      
+        <div className="loader-container">
+          <img src={loader} alt="loading icon" />
+        </div>
+          <p className="city-name">Paris</p>
+          <p className="country-name">France</p>
+          <p className="temperature">17°</p>
+          <div>
+            <img src="/icons/01d.svg" alt="weather icon" className="info-icon"/>
+          </div>
       </main>
   
   );
