364cb020ae4dc30c6598c37ae533b21afcddb75f
Benjamin AUBRON <benjamin.aubron@outlook.com>
2024-09-06 16:01:59 +0200

    projet - done

--- 8.infinite-scroll/src/App.jsx
+++ 8.infinite-scroll/src/App.jsx
@@ -1,8 +1,11 @@
+import List from "./components/List"
 
 function App() {
   return (
-    <div>
-  
+    <div className="bg-gray-50 min-h-screen">
+      <div className="max-w-4xl mx-auto pt-20 px-4">
+        <List/>
+      </div>
     </div>
   )
 }
